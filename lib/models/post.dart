ed7165283a88695bb5f27af1f5096d63a42a88fd
devanandr001 <devanand02r@gmail.com>
2023-09-10 17:30:31 +0530

    post.dart

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -1,4 +1,41 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 
-// asdfas
+class Post{
+  final String description;
+  final String uid;
+  final String username;
+  final likes;
+  final String postId;
+  final DateTime datePublished;
+  final String postUrl;
+  final String profImage;
+
+  const Post(
+  {
+    required this.description,
+    required this.uid,
+    required this.username,
+    required this.likes,
+    required this.postId,
+    required this.datePublished,
+    required this.postUrl,
+    required this.profImage,
+  });
+
+  static Post fromSnap(DocumentSnapshot snap) {
+    var snapshot = snap.data() as Map<String, dynamic>;
+
+    return Post(
+      description: snapshot["description"],
+      uid: snapshot["uid"],
+      likes: snapshot["likes"],
+      postId: snapshot["postId"],
+      datePublished: snapshot["dataPublished"],
+      username: snapshot["username"],
+      postUrl: snapshot['postUrl'],
+      profImage: snapshot['profImage']
+    );
+  }
+
+}
